8904ff374f5858c20507030580c48380a2412276
Aditya Chakraborty <78040114+adityackr@users.noreply.github.com>
2021-08-31 16:27:22 +0600

    auth started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Fragment } from 'react';
 import Counter from './components/Counter';
-
+import Header from './components/Header';
+import Auth from './components/Auth';
 
 function App() {
-  return (
-    <Counter />
-  );
+    return (
+        <Fragment>
+            <Header />
+            <Auth />
+        </Fragment>
+    );
 }
 
 export default App;
