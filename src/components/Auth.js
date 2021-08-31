741587c1811944e17cd5ac2ca8ce7b78edf8bf27
Aditya Chakraborty <78040114+adityackr@users.noreply.github.com>
2021-08-31 17:30:23 +0600

    validation added

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { authActions } from '../store/auth';
 import classes from './Auth.module.css';
@@ -5,23 +6,99 @@ import classes from './Auth.module.css';
 const Auth = () => {
     const dispatch = useDispatch();
 
-    const loginHandler = () => {
-        dispatch(authActions.login());
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [emailTouched, setEmailTouched] = useState(false);
+
+    const [enteredPassword, setEnteredPassword] = useState('');
+    const [passwordTouched, setPasswordTouched] = useState(false);
+
+    const enteredEmailIsValid = enteredEmail.includes('@');
+    const emailIsInvalid = !enteredEmailIsValid && emailTouched;
+
+    const enteredPasswordIsValid =
+        enteredPassword !== '' && enteredPassword.length >= 8;
+    const passwordIsInvalid = !enteredPasswordIsValid && passwordTouched;
+
+    let formIsValid = false;
+
+    if (enteredEmailIsValid && enteredPasswordIsValid) {
+        formIsValid = true;
+    }
+
+    const emailChangeHandler = (event) => {
+        setEnteredEmail(event.target.value);
     };
+
+    const emailBlurHandler = () => {
+        setEmailTouched(true);
+    };
+
+    const passwordChangeHandler = (event) => {
+        setEnteredPassword(event.target.value);
+    };
+
+    const passwordBlurHandler = () => {
+        setPasswordTouched(true);
+    };
+
+    const loginHandler = (event) => {
+        event.preventDefault();
+        setEmailTouched(true);
+        setPasswordTouched(true);
+
+        if (!enteredEmailIsValid && !enteredPasswordIsValid) {
+            return;
+        }
+
+        dispatch(authActions.login());
+
+        setEmailTouched(false);
+        setPasswordTouched(false);
+    };
+
+    const emailInputClasses = `${classes.control} ${
+        emailIsInvalid ? classes.invalid : ''
+    }`;
+    const passwordInputClasses = `${classes.control} ${
+        passwordIsInvalid ? classes.invalid : ''
+    }`;
 
     return (
         <main className={classes.auth}>
             <section>
                 <form onSubmit={loginHandler}>
-                    <div className={classes.control}>
+                    <div className={emailInputClasses}>
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" />
+                        <input
+                            type="email"
+                            id="email"
+                            onChange={emailChangeHandler}
+                            onBlur={emailBlurHandler}
+                        />
+                        {emailIsInvalid && (
+                            <p className={classes.error}>
+                                <strong>Please enter a valid email.</strong>
+                            </p>
+                        )}
                     </div>
-                    <div className={classes.control}>
+                    <div className={passwordInputClasses}>
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" />
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={passwordChangeHandler}
+                            onBlur={passwordBlurHandler}
+                        />
                     </div>
-                    <button>Login</button>
+                    {passwordIsInvalid && (
+                        <p className={classes.error}>
+                            <strong>
+                                Please enter a password of at least 8
+                                characters.
+                            </strong>
+                        </p>
+                    )}
+                    <button disabled={!formIsValid}>Login</button>
                 </form>
             </section>
         </main>
